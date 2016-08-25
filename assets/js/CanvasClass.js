5fe8db7cc9ddc528b63ee6ec2d4d9b4a9e451254
fortune <legalmody@gmail.com>
2016-08-25 22:29:05 +0100

    add a class method for binding settings and changes also remove bootstrap cdn

--- assets/js/CanvasClass.js
+++ assets/js/CanvasClass.js
@@ -22,9 +22,8 @@ CanvasClass.prototype.initStrokeWidth = function(){
 /**
 * Drawing modes change binding
 */
-
 CanvasClass.prototype.bindDrawingModesChangeListener = function(){
-    var $this = this ;
+    var $this = this;
     var items = document.getElementsByClassName('drawingTool');
 
     var binder = function(){
@@ -38,5 +37,52 @@ CanvasClass.prototype.bindDrawingModesChangeListener = function(){
     for(var i=0; i<items.length; i++){
         items[i].addEventListener('click', binder, false);
     }
+};
 
+/**
+*Binding settings and changes events
+*/
+CanvasClass.prototype
+  .bindSettingsChangeListenters = function(){
+    var $this = this ;
+
+    /* Setup eraser */
+    document.getElementById('eraser')
+      .addEventListener('click', function(){
+        $this.drawMode = this.getAttribute("data-title");
+    });
+
+    /* Event for setting pencil stroke & fill color */
+    document.getElementById('pencilDraw')
+      .addEventListener('click', function(){
+        var pencilColor = document.getElementById('pencilColor').value;
+        $this.drawMode = this.getAttribute("data-title");
+        $this.setFillcolor(pencilColor);
+        $this.setStrokeColor(pencilColor);
+    });
+
+    /* Changes stroke width value */
+    document.getElementById('strokeRadius')
+      .addEventListener('change', function(){
+        $this.setStrokeWidth(this.value);
+    });
+
+    /* Changes stroke color value */
+    document.getElementById('strokeColor')
+      .addEventListener('change', function(){
+        $this.setStrokeColor(this.value);
+    });
+
+    /* Changes fill color value */
+    document.getElementById('fillColor')
+      .addEventListener('change', function(){
+        $this.setFillcolor(this.value);
+    });
+
+    /* Changes pencil color value */
+    document.getElementById('pencilColor')
+      .addEventListener('change', function(){
+        $this.setFillcolor(this.value);
+        $this.setStrokeColor(this.value);
+    });
 };
