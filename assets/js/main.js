73287b7655cacda820bb83394e9081968ee69166
Fortune Ekeruo <legalmody@gmail.com>
2018-08-08 17:43:59 +0100

    on mouse down we engage our canvas and on mouse we disengage

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,24 @@ const canvas = document.getElementById('canvas');
 let painting;
 let mouseDrag = false;
 
+/**
+ * on mousedown we engage our canvas
+ * @param {object} e 
+ */
+const engage = (e) => {
+    mouseDrag = true;
+    painting.lineDraw(e);
+}
+
+/**
+ * On mouseup we disengage and begin a new path
+ * @param {object} e 
+ */
+const disengage = (e) => {
+    mouseDrag = false;
+    painting.context.beginPath();
+}
+
 const initializePainting = (canvas) => {
     painting = new Paint(canvas);
     painting.initStrokeWidth();
